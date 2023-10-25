c64c595ff1cf72acf487da53a8a12f4a9f556c5f
DoanMinh <91416001+dbn-minh@users.noreply.github.com>
2023-10-25 11:45:19 +0700

    Add files via upload

---
+++ BattleShipGame/project/src/Rectangle.java
@@ -0,0 +1,39 @@
+
+/*Game: Battleship
+  Rectangle Class:
+  A simple Rectangle with a position defined by the top left corner,
+  and a width,height to represent the size of the Rectangle
+ */ 
+
+public class Rectangle {
+    protected Position position;
+    protected int width;
+    protected int height;
+/* The first constructor of the Rectangle */
+    public Rectangle(Position position, int width, int height) {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+    }
+/* The second constructor of the Rectangle */
+    public Rectangle(int x, int y, int width, int height) {
+        this(new Position(x,y), width, height);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    /*Test if the targetPosition is inside the Rectangle*/
+    public boolean isPositionInside(Position targetPosition) {
+        return targetPosition.x >= position.x && targetPosition.y >= position.y && targetPosition.x < position.x + width && targetPosition.y < position.y + height;
+    }
+}
