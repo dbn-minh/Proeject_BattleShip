c772a94099ec0e155c1a5a57ff8f095ea5deb7ca
triphuc <panshine15@gmail.com>
2023-12-25 08:51:18 +0700

    update

--- BattleShipGame/project/src/Marker.java
+++ BattleShipGame/project/src/Marker.java
@@ -1,51 +1,56 @@
 import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * Marker class:
- * Represents a simple coloured rectangle that can be either
- * shown or hidden, and will change colour based on whether
- * it is representing the location where a ship is.
- */
 public class Marker extends Rectangle {
-    /**
-     * The colour to show when a ship is located at this marker.
-     */
+    private JLabel imageLabel;
+
     private final Color HIT_COLOUR = new Color(219, 23, 23, 180);
-    /**
-     * The colour to show when there is no ship at this marker.
-     */
+
     private final Color MISS_COLOUR = new Color(26, 26, 97, 180);
-    /**
-     * Padding around the edges of the filled rectangle to make it a little smaller.
-     */
+
     private final int PADDING = 3;
-    /**
-     * When true the marker will be painted.
-     */
+
+    private List<ImageIcon> hitImages; // Danh sách các hình ảnh để hiển thị khi tàu bị đánh
+    private int currentImageIndex;
+
     private boolean showMarker;
-    /**
-     * Changes the colour used for drawing. When a ship is assigned will use HIT_COLOUR,
-     * when null will use MISS_COLOUR.
-     */
+    private Timer explosionTimer;
+    private static final int EXPLOSION_DELAY = 100;
+
+    private boolean continuousHit;
+    private Thread continuousImageUpdateThread;
+
     private Ship shipAtMarker;
 
-    /**
-     * Prepares the marker with a default state where it is ready to draw
-     * at a specified position with no associated ship by default.
-     *
-     * @param x X coordinate to draw this marker at.
-     * @param y Y coordinate to draw this marker at.
-     * @param width Width of the marker's cell.
-     * @param height Height of the marker's cell.
-     */
+    private ImageIcon hitImage;
+
     public Marker(int x, int y, int width, int height) {
         super(x, y, width, height);
+        hitImages = new ArrayList<>();
+        hitImages.add(new ImageIcon("BattleShipGame/project/Image/explosion0.png"));
+        hitImages.add(new ImageIcon("BattleShipGame/project/Image/explosion1.png"));
+        hitImages.add(new ImageIcon("BattleShipGame/project/Image/explosion2.png"));
+        hitImages.add(new ImageIcon("BattleShipGame/project/Image/explosion3.png"));
+        hitImages.add(new ImageIcon("BattleShipGame/project/Image/explosion4.png"));
+
+        explosionTimer = new Timer(EXPLOSION_DELAY, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateContinuousImage();
+                repaint();
+            }
+        });
+        explosionTimer.setRepeats(true);
+
         reset();
     }
 
-    /**
-     * Resets to no referenced ship, and with the marker not visible.
-     */
     public void reset() {
         shipAtMarker = null;
         showMarker = false;
@@ -57,10 +62,14 @@ public class Marker extends Rectangle {
      * it visible for drawing.
      */
     public void mark() {
-        if(!showMarker && isShip()) {
+        if (!showMarker && isShip()) {
             shipAtMarker.destroySection();
         }
         showMarker = true;
+        currentImageIndex = (currentImageIndex + 1) % hitImages.size();
+        continuousImageUpdateThread = new Thread(new ContinuousImageUpdate());
+        continuousImageUpdateThread.start();
+        explosionTimer.start();
     }
 
     /**
@@ -101,6 +110,40 @@ public class Marker extends Rectangle {
         return shipAtMarker;
     }
 
+    public void stopContinuousHit() {
+        continuousHit = false; // Stop the continuous hit thread
+        continuousImageUpdateThread = null; // Release the reference to the thread
+    }
+
+    private class ContinuousImageUpdate implements Runnable {
+        @Override
+        public void run() {
+            while (continuousHit) {
+                try {
+                    // Pause for a short duration
+                    Thread.sleep(100); // Đặt khoảng thời gian giữa các hình ảnh ở đây (0.1 giây trong ví dụ)
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                updateContinuousImage(); // Update the image continuously
+                repaint(); // Yêu cầu vẽ lại để cập nhật hình ảnh mới trên giao diện
+            }
+        }
+    }
+
+    // public void updateContinuousImage() {
+    // currentImageIndex = (currentImageIndex + 1) % hitImages.size();
+    // }
+    private void updateContinuousImage() {
+        if (currentImageIndex < hitImages.size() - 1) {
+            currentImageIndex++;
+        } else {
+            // Stop the timer when all frames have been drawn
+            stopContinuousHit();
+        }
+    }
+
     /**
      * Does nothing if not marked.
      * Draws a rectangle to match the correct padded size of the marker.
@@ -109,9 +152,23 @@ public class Marker extends Rectangle {
      * @param g Reference to the Graphics object for drawing.
      */
     public void paint(Graphics g) {
-        if(!showMarker) return;
+        if (!showMarker)
+            return;
 
-        g.setColor(isShip() ? HIT_COLOUR : MISS_COLOUR);
-        g.fillRect(position.x+PADDING+1, position.y+PADDING+1, width-PADDING*2, height-PADDING*2);
+        if (isShip() && currentImageIndex < hitImages.size()) {
+            ImageIcon currentImage = hitImages.get(currentImageIndex);
+            int xOffset = PADDING + 1;
+            int yOffset = PADDING + 1;
+
+            g.drawImage(currentImage.getImage(), position.x + xOffset, position.y + yOffset,
+                    width - PADDING * 2, height - PADDING * 2, null);
+        } else {
+            // Vẽ một hình chữ nhật với màu để thể hiện lỗi đánh
+            g.setColor(MISS_COLOUR);
+            g.fillRect(position.x + PADDING + 1, position.y + PADDING + 1,
+                    width - PADDING * 2, height - PADDING * 2);
+        }
+
     }
+
 }
